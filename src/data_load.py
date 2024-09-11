1b11417dc092a63b06ad1c6b323cce7d51157cff
Wang Haowei <hw_wang@whu.edu.cn>
2024-09-11 21:00:41 +0800

    fix the path wrong in windows system

--- src/data_load.py
+++ src/data_load.py
@@ -3,10 +3,15 @@ a script to load data from different sources and save it to csv
 folder path is ./data
 """
 
+from pathlib import Path
 from typing import List
 
 import matplotlib.pyplot as plt
 from datasets import Dataset, load_dataset
+
+from proxy import set_proxy
+
+set_proxy()
 
 
 def rename(dataset: Dataset, candidate_column: List[str], new_column: str) -> Dataset:
@@ -30,7 +35,7 @@ def rename(dataset: Dataset, candidate_column: List[str], new_column: str) -> Da
     dataset = dataset.rename_columns({col: col.lower() for col in dataset.column_names})
     fields = []
     for name in candidate_column:
-        if name.lower() in map(str.lower, dataset.column_names):
+        if name.lower() in dataset.column_names:
             fields.append(name)
     if len(fields) == 1:
         answer_field = fields[0]
@@ -48,101 +53,6 @@ def rename(dataset: Dataset, candidate_column: List[str], new_column: str) -> Da
             dataset = dataset.map(lambda x: {new_column: x[answer_field]})
         else:
             dataset = dataset.rename_column(answer_field, new_column)
-    return dataset
-
-
-def load_zh(
-    task: str,
-    from_remote: bool = True,
-) -> Dataset:
-    """
-    Load a dataset for a specified task in Chinese.
-
-    Args:
-        task (str): The task for which to load the dataset. Must be one of
-                    ["medical", "financial", "legal", "code"].
-        from_remote (bool, optional): If True, load the dataset from a remote source.
-                                      If False, load the dataset from a local JSON file.
-                                      Defaults to True.
-
-    Returns:
-        Dataset: The loaded dataset.
-
-    Raises:
-        ValueError: If the specified task is not one of the valid options.
-    """
-    if from_remote:
-        if task in ["medical", "financial", "legal"]:
-            dataset = load_dataset(
-                "wanghw/human-ai-comparison",
-                verification_mode="no_checks",
-                split="train",
-            )
-            dataset = dataset.filter(
-                lambda x: x["field"] == task
-                and x["label"] == "human"
-                and x["question"] is not None
-            )
-        elif task == "code":
-            dataset = load_dataset(
-                "jean1/45k_python_code_chinese_instruction", split="train"
-            )
-        else:
-            raise ValueError("Invalid task")
-        dataset = rename(dataset, ["output", "answer", "response", "body"], "answer")
-        dataset = rename(
-            dataset,
-            ["input", "question", "response", "body", "selftext", "instruction"],
-            "question",
-        )
-    else:
-        dataset = load_dataset("json", data_files=f"data/zh_{task}.json", split="train")
-    return dataset
-
-
-def load_en(
-    task: str,
-    from_remote: bool = True,
-) -> Dataset:
-    """
-    Load an English dataset based on the specified task.
-
-    Parameters:
-    - task (str): The type of dataset to load. Must be one of "medical", "financial", "legal", or "code".
-    - from_remote (bool, optional): If True, load the dataset from a remote source. If False, load the dataset from a local JSON file. Default is True.
-
-    Returns:
-    - dataset: The loaded dataset.
-
-    Raises:
-    - ValueError: If the specified task is not one of the allowed values.
-    """
-    if from_remote:
-        if task == "medical":
-            dataset = load_dataset(
-                "Malikeh1375/medical-question-answering-datasets",
-                "all-processed",
-                split="train",
-            )
-            dataset = dataset.rename_column("input", "question")
-        elif task == "financial":
-            dataset = load_dataset("winddude/reddit_finance_43_250k", split="train")
-        elif task == "legal":
-            dataset = load_dataset("ibunescu/qa_legal_dataset_val", split="validation")
-        elif task == "code":
-            dataset = load_dataset(
-                "iamtarun/python_code_instructions_18k_alpaca", split="train"
-            )
-        else:
-            raise ValueError("Invalid task")
-        dataset = rename(dataset, ["output", "answer", "response", "body"], "answer")
-        dataset = rename(
-            dataset, ["input", "question", "response", "body", "selftext"], "question"
-        )
-    else:
-        dataset = load_dataset(
-            "json", data_files=f"data/en_{task}.json", split="train", force_ascii=False
-        )
     return dataset
 
 
@@ -171,24 +81,68 @@ def load_qa(
     Raises:
         ValueError: If an invalid language is specified.
     """
-    if language == "zh":
-        dataset = load_zh(task, from_remote)
-    elif language == "en":
-        dataset = load_en(task, from_remote)
+    filename = Path.cwd().resolve().joinpath(Path(f"data/{language}_{task}.json"))
+    if from_remote:
+        if language == "zh":
+            if task in ["medical", "financial", "legal"]:
+                dataset = load_dataset(
+                    "wanghw/human-ai-comparison",
+                    verification_mode="no_checks",
+                    split="train",
+                )
+                dataset = dataset.map(lambda x: {"answer": x["answer"].strip("[]")})
+                dataset = dataset.filter(
+                    lambda x: x["field"] == task
+                    and x["label"] == "human"
+                    and x["question"] is not None
+                )
+            elif task == "code":
+                dataset = load_dataset(
+                    "jean1/45k_python_code_chinese_instruction", split="train"
+                )
+            else:
+                raise ValueError("Invalid task")
+        elif language == "en":
+            if from_remote:
+                if task == "medical":
+                    dataset = load_dataset(
+                        "Malikeh1375/medical-question-answering-datasets",
+                        "all-processed",
+                        split="train",
+                    )
+                    dataset = dataset.rename_column("input", "question")
+                elif task == "financial":
+                    dataset = load_dataset(
+                        "winddude/reddit_finance_43_250k", split="train"
+                    )
+                elif task == "legal":
+                    dataset = load_dataset(
+                        "ibunescu/qa_legal_dataset_val", split="validation"
+                    )
+                elif task == "code":
+                    dataset = load_dataset(
+                        "iamtarun/python_code_instructions_18k_alpaca", split="train"
+                    )
+                else:
+                    raise ValueError("Invalid task")
+        else:
+            raise ValueError("Invalid language")
     else:
-        raise ValueError("Invalid language")
+        dataset = load_dataset("json", data_files=filename, split="train")
+
+    dataset = rename(dataset, ["output", "answer", "response", "body"], "answer")
+    dataset = rename(
+        dataset, ["input", "question", "body", "selftext", "instruction"], "question"
+    )
     dataset = dataset.filter(lambda x: min_length <= len(x["question"]) <= max_length)
     if count is not None and len(dataset) > count:
         dataset = dataset.select(range(count))
+        dataset = dataset.remove_columns(
+            [col for col in dataset.column_names if col not in ["question", "answer"]]
+        )
+        dataset.to_json(filename, force_ascii=False, lines=True)
     else:
         print("Not enough data to select and load from the remote")
-        if language == "zh":
-            dataset = load_zh(task, True)
-        elif language == "en":
-            dataset = load_en(task, True)
-        else:
-            raise ValueError("Invalid language")
-    dataset.to_json(f"data/{language}_{task}.json", orient="records", lines=True)
     return dataset["question"], dataset["answer"]
 
 
@@ -222,6 +176,6 @@ if __name__ == "__main__":
         "legal",
     ]:
         print(f"Task: {task}")
-        for language in ["zh", "en"]:
+        for language in ["zh"]:
             print(f"Language: {language}")
-            result = load_qa(language, task, 20, 1000, 5000, False)
+            result = load_qa(language, task, 20, 1000, 1000, True)
