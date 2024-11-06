4662958e8d3e1cd7af371e89b86d6cdfd10c1208
Wang Haowei <hw_wang@whu.edu.cn>
2024-11-06 21:47:45 +0800

    fix some wrong

--- src/llm_evaluation/chart.py
+++ src/llm_evaluation/chart.py
@@ -602,7 +602,7 @@ def main():
         encoding="utf-8",
     ) as config_file:
         config = yaml.safe_load(config_file)  # noqa: F821
-    draw_score(config=config, metric_list=["cosine", "rouge1"],suffix='eps')
+
     # draw_length_distribution(config=config)
     # draw_tsne(config=config, suffix="eps")
 
